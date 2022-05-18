5a87ad155346e5b8a13e8f9ae0e4bad46f64e1d3
Vinaigre <olivier.savarino+sufod@gmail.com>
2022-05-18 16:13:05 +0200

    partial word files

--- scripts/buildPartialFiles.sh
+++ scripts/buildPartialFiles.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo todo
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FULL_FILES_PATH="${DIR}/../data/full"
+PARTIAL_FILES_PATH="${DIR}/../data/partial"
+
+DOFUS_FILE="${FULL_FILES_PATH}/dofus.txt"
+FRENCH_FILE="${FULL_FILES_PATH}/french.txt"
+
+rm "$PARTIAL_FILES_PATH/"*".txt"
+
+while IFS= read -r line; do
+  length=${#line}
+  echo "$line" >> "$PARTIAL_FILES_PATH/dofus${length}.txt"
+done < "${DOFUS_FILE}"
+
+while IFS= read -r line; do
+  length=${#line}
+  echo "$line" >> "$PARTIAL_FILES_PATH/french${length}.txt"
+done < "${FRENCH_FILE}"
