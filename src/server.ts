2bd5b9f4b91559707430795a5b75f4f6f540f116
Vinaigre <olivier.savarino+sufod@gmail.com>
2022-05-18 16:13:05 +0200

    Working keyboard events

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+import * as express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+const port = 8080;
+const publicDir = __dirname + '/public';
+
+app.use('/public', express.static(publicDir));
+
+app.get('/', function(req: Request, res: Response) {
+  res.sendFile(publicDir + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
