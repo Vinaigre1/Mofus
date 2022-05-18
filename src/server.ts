28b5403005212e7ce6010396a605fe89f48ef466
Vinaigre <olivier.savarino+sufod@gmail.com>
2022-05-18 16:13:05 +0200

    Finished first iteration of words

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,11 @@ app.get('/', (req: Request, res: Response) => {
   res.sendFile(publicDir + '/index.html');
 });
 
+app.get('/list', (req: Request, res: Response) => {
+  const length = +req.query.length;
+  res.send(typeof length + '<br>' + length);
+});
+
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
